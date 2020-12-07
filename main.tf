65acdbb7d35ee9be2282cbe1b917bf20caf7f064
Smashkat12 <smashkat12@gmail.com>
2020-12-07 21:47:00 +0200

    basic outline of infra that to be created

--- main.tf
+++ main.tf
@@ -9,5 +9,24 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "af-south-1"
+  access_key = ""
+  secret_key = ""
 }
 
+# Query all avilable Availibility Zone
+data "aws_availability_zones" "available" {}
+
+
+# VPC Creation
+# Creating Internet Gateway
+# Public Route Table
+# Private Route Table
+# Public Subnet
+# Private Subnet
+# Associate Public Subnet with Public Route Table
+# Associate Private Subnet with Private Route Table
+# Security Group Creation
+# Ingress Security Port 22
+# Ingress Security Port 80 or 8080
+# Egress Security All OutBound Access
+# Adding Route for Transit Gateway
