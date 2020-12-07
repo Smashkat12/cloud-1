0c194ca154328215e6456c219ca00733623ebebe
Smashkat12 <smashkat12@gmail.com>
2020-12-07 22:41:20 +0200

    associated subnets and route tables

--- main.tf
+++ main.tf
@@ -93,15 +93,38 @@ resource "aws_subnet" "my_cloud1_private_subnet" {
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
 
   tags = {
-    Name = "my-test-private-subnet.${count.index + 1}"
+    Name = "my_cloud1_private_subnet.${count.index + 1}"
   }
 }
 
 
 # Associate Public Subnet with Public Route Table
+
+resource "aws_route_table_association" "public_subnet_assoc" {
+  count          = 2
+  route_table_id = "${aws_route_table.my_public_route_table.id}"
+  subnet_id      = "${aws_subnet.my_cloud1_public_subnet.*.id[count.index]}"
+  depends_on     = ["aws_route_table.my_public_route_table", "aws_subnet.my_cloud1_public_subnet"]
+}
+
 # Associate Private Subnet with Private Route Table
+resource "aws_route_table_association" "private_subnet_assoc" {
+  count          = 2
+  route_table_id = "${aws_default_route_table.my_private_route_table.id}"
+  subnet_id      = "${aws_subnet.my_cloud1_private_subnet.*.id[count.index]}"
+  depends_on     = ["aws_default_route_table.my_private_route_table", "aws_subnet.my_cloud1_private_subnet"]
+}
 # Security Group Creation
+resource "aws_security_group" "my_cloud1_sg" {
+  name   = "my_cloud1_sg"
+  vpc_id = "${aws_vpc.my_cloud1_vpc.id}"
+}
+
 # Ingress Security Port 22
+
+
+
+
 # Ingress Security Port 80 or 8080
 # Egress Security All OutBound Access
 # Adding Route for Transit Gateway
