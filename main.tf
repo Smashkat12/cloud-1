afdcab87e8f2ca5146f30fb5a09893d42f2428e1
Smashkat12 <smashkat12@gmail.com>
2020-12-07 22:31:51 +0200

    route tables and subnets

--- main.tf
+++ main.tf
@@ -24,27 +24,80 @@ resource "aws_vpc" "my_cloud1_vpc" {
   enable_dns_support = true
 
   tags = {
-    Name = "my-cloud1-vpc"
+    Name = "my_cloud1_vpc"
   }
 }
 
 
 
 # Creating Internet Gateway
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "my_cloud1_igw" {
   vpc_id = "${aws_vpc.my_cloud1_vpc.id}"
 
   tags = {
-    Name = "my-cloud1-igw"
+    Name = "my_cloud1_igw"
   }
 }
 
 
 
 # Public Route Table
+resource "aws_route_table" "my_public_route_table" {
+  vpc_id = "${aws_vpc.my_cloud1_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.my_cloud1_igw.id}"
+  }
+
+  tags = {
+    Name = "my_public_route_table"
+  }
+}
+
+
 # Private Route Table
+resource "aws_default_route_table" "my_private_route_table" {
+  default_route_table_id = "${aws_vpc.my_cloud1_vpc.default_route_table_id}"
+
+  route {
+    nat_gateway_id = "${aws_nat_gateway.my_cloud1_nat_gateway.id}"
+    cidr_block     = "0.0.0.0/0"
+  }
+
+  tags = {
+    Name = "my_private_route_table"
+  }
+}
+
+
 # Public Subnet
+resource "aws_subnet" "my_cloud1_public_subnet" {
+  count                   = 2
+  cidr_block              = "${var.public_cidrs[count.index]}"
+  vpc_id                  = "${aws_vpc.my_cloud1_vpc.id}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my_cloud1_public_subnet.${count.index + 1}"
+  }
+}
+
+
 # Private Subnet
+resource "aws_subnet" "my_cloud1_private_subnet" {
+  count             = 2
+  cidr_block        = "${var.private_cidrs[count.index]}"
+  vpc_id            = "${aws_vpc.my_cloud1_vpc.id}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my-test-private-subnet.${count.index + 1}"
+  }
+}
+
+
 # Associate Public Subnet with Public Route Table
 # Associate Private Subnet with Private Route Table
 # Security Group Creation
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "vpc_cidr" {
 	default = "10.0.0.0/16"
 }
+
+variable "public_cidrs" {
+  type = "list"
+}
+
+variable "private_cidrs" {
+  type = "list"
+}
+
+variable "transit_gateway" {}
